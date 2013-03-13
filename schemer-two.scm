4e1f16fb9c99f14f801bc36430470ea7e493b3e5
Ante Salinovic <nikuda@gmail.com>
2013-03-13 23:33:01 +1100

    Added eqlist?.

--- schemer-two.scm
+++ schemer-two.scm
@@ -334,9 +334,29 @@
 
 (leftmost '((leftiest) (yes (test test))))
 
-;; or   -   asks questions one at a time until it finds one that is true
-;;          returns bool
+; or   -   asks questions one at a time until it finds one that is true
+;          returns bool
 
-;; page 91
+; page 91
 
-;; (define eqlist?)
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((and (null? l1) (atom? (car l2))) #f)
+      ((null? l1) #f)
+      ((and (atom? (car l1)) (null? l2)) #f)
+      ((and (atom? (car l1)) (atom? (car l2)))
+       (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+      ((atom? (car l1)) #f)
+      ((null? l2) #f)
+      ((atom? (car l2)) #f)
+      (else
+        (and (eqlist? (car l1) (car l2))
+          (eqlist? (cdr l1) (cdr l2)))))))
+
+(eqlist? '() '())
+(eqlist? '() '(abc))
+(eqlist? '(abc) '(abc))
+(eqlist? '() '(()))
+(eqlist? '(()) '())
